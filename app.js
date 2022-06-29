c38133ee3758c4ae12613859168b95ab3772b1af
Aladdin4u <taofikakinkumi@gmail.com>
2022-06-29 20:48:16 +0100

    first commit

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const blogRoutes = require('./routes/blogRoutes')
-const _ = require('lodash')
 require('dotenv').config()
 
 
 // connect to mongodb
 const dbURI = process.env.DB_STRING
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then((result) => app.listen(2022))
+  .then((result) => {
+    app.listen(2022)
+    console.log('connected to database') 
+  })
   .catch((err) => console.log(err))
 
 app.set('view engine', 'ejs')
