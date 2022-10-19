84f33ee6e72d13c0ca20be9af0097c278aaa14d5
aladdin4u <taofikakinkumi@gmail.com>
2022-10-19 04:25:52 +0100

    add dotenv path

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const flash = require("express-flash");
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
-require('dotenv').config()
+require('dotenv').config({ path: "./config/.env" })
 const connectDB = require('./config/db')
 const mainRoutes = require('./routes/auth')
 const repRoutes = require('./routes/reports')
