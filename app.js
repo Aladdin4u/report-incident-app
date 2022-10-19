c95e5473494a22618ad9df9afc2d334efa456fce
aladdin4u <taofikakinkumi@gmail.com>
2022-10-19 06:16:16 +0100

    init client mongo connect

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false,
-  store: MongoStore.create({ mongoUrl:process.env.DB_STRING })
+  store: MongoStore.create({ mongoUrl:process.env.DB_STRING, touchAfter: 24 * 3600 })
 }))
 
 // Passport middleware
