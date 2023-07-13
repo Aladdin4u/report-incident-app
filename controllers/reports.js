7bf7cae47000bfcfd4eb6b4c74e252dd371915e4
Aladdin4u <taofikakinkumi@gmail.com>
2023-07-13 23:16:11 +0100

    refactor code

---
+++ controllers/reports.js
@@ -0,0 +1,145 @@
+const cloudinary = require("../middleware/cloudinary");
+const Report = require("../models/Report");
+
+module.exports = {
+  getDashboard: async function (req, res) {
+    try {
+      const users = req.user;
+      const reports = await Report.find({ user: req.user.id })
+        .sort({ createdAt: "desc" })
+        .lean();
+      res.render("dashboard.ejs", {
+        reports,
+        title: "dashboard",
+        name: req.user.userName,
+        users,
+      });
+    } catch (err) {
+      console.log(err);
+      res.render("error/404");
+    }
+  },
+  reportIndex: async function (req, res) {
+    try {
+      const users = req.user;
+      const reports = await Report.find()
+        .populate("user")
+        .sort({ createdAt: "desc" })
+        .lean();
+  
+      res.render("reports/index", { reports, title: "Home", users });
+    } catch (err) {
+      console.error(err);
+      res.render("/error/404");
+    }
+  },
+  reportDetails: async function (req, res)  {
+    try {
+      const users = req.user;
+      const report = await Report.findById({ _id: req.params.id })
+        .populate("user")
+        .lean();
+  
+      if (!report) {
+        return res.render("/error/404");
+      }
+      res.render("reports/details", { report, title: "Report Details", users });
+    } catch (err) {
+      console.error(err);
+      res.render("/error/404", { title: "Report not found" });
+    }
+  },
+  reportCreateGet: async function (req, res) {
+    const users = req.user;
+    res.render("reports/create", { title: "Create a new Report", users });
+  },
+  reportCreatePost: async function (req, res) {
+    try {
+      const users = req.user;
+      req.body.user = req.user.id;
+      // Upload image to cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+      console.log(result);
+  
+      await Report.create({
+        title: req.body.title,
+        image: result.secure_url,
+        cloudinaryId: result.public_id,
+        withness: req.body.withness,
+        tbody: req.body.tbody,
+        user: req.user.id,
+      });
+      res.redirect("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  reportDelete: async function (req, res)  {
+    try {
+      const users = req.user;
+      // Find post by id
+      let report = await Report.findById({ _id: req.params.id });
+      // Delete image from cloudinary
+      await cloudinary.uploader.destroy(report.cloudinaryId);
+      // Delete report from database
+      await Report.remove({ _id: req.params.id });
+      res.redirect("/dashboard");
+    } catch (err) {
+      console.error(err);
+      res.redirect("/error/404");
+    }
+  },
+  reportEdit: async function (req, res) {
+    const users = req.user;
+    const report = await Report.findOne({ _id: req.params.id }).lean();
+  
+    if (!report) {
+      return res.render("/error/404");
+    }
+  
+    if (report.user != req.user.id) {
+      res.redirect("/report");
+    } else {
+      res.render("reports/edit", { report, title: "Edit Report", users });
+    }
+  },
+  reportUpdateEdit: async function (req, res) {
+    try {
+      const users = req.user;
+      const report = await Report.findById({ _id: req.params.id }).lean();
+  
+      if (!report) {
+        return res.render("error/404");
+      }
+  
+      if (report.user != req.user.id) {
+        res.redirect("/reports");
+      } else {
+        let report = await Report.findById({ _id: req.params.id });
+        // Delete image from cloudinary
+        await cloudinary.uploader.destroy(report.cloudinaryId);
+        // Upload image to cloudinary
+        const result = await cloudinary.uploader.upload(req.file.path);
+  
+        const updates = {
+          title: req.body.title || report.title,
+          withness: req.body.withness || report.withness,
+          image: result.secure_url || report.image,
+          cloudinaryId: result.public_id || report.cloudinaryId,
+          tbody: req.body.tbody || report.tbody,
+          user: req.user.id,
+        };
+  
+        report = await Report.findByIdAndUpdate({ _id: req.params.id }, updates, {
+          new: true,
+          runValidators: true,
+        });
+        // console.log(report)
+        res.redirect("/dashboard");
+      }
+    } catch (err) {
+      console.error(err);
+      res.redirect("error/404");
+    }
+  },
+};
--- routes/reports.js
+++ routes/reports.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const upload = require("../middleware/multer");
-const rController = require('../controllers/rController')
+const reports = require('../controllers/reports')
 const { ensureAuth } = require('../middleware/auth')
 const router = express.Router();
 
-router.get('/', rController.report_index)
-router.post('/', ensureAuth, upload.single("file"), rController.report_create_post)
-router.get('/create', ensureAuth, rController.report_create_get)
-router.get('/:id', ensureAuth, rController.report_details)
-router.get('/edit/:id', ensureAuth, rController.report_edit)
-router.put('/edit/:id', ensureAuth, upload.single("file"), rController.report_up_edit)
-router.delete('/:id', ensureAuth, rController.report_delete)
+router.get('/', reports.reportIndex)
+router.post('/', ensureAuth, upload.single("file"), reports.reportCreatePost)
+router.get('/create', ensureAuth, reports.reportCreateGet)
+router.get('/:id', ensureAuth, reports.reportDetails)
+router.get('/edit/:id', ensureAuth, reports.reportEdit)
+router.put('/edit/:id', ensureAuth, upload.single("file"), reports.reportUpdateEdit)
+router.delete('/:id', ensureAuth, reports.reportDelete)
 
 module.exports = router;
