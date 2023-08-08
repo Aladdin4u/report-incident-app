f8252637b73f420f6aca62d1895ff03764c93a22
Aladdin4u <taofikakinkumi@gmail.com>
2023-08-08 23:55:44 +0100

    send email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,10 +1,35 @@
-const nodemailer = require('nodemailer');
-const nodemailerSendgrid = require('nodemailer-sendgrid');
+const nodemailer = require("nodemailer");
 
-const transport = nodemailer.createTransport(
-    nodemailerSendgrid({
-        apiKey: process.env.SENDGRID_API_KEY
-    })
-);
+let config = {
+  service : 'gmail',
+  auth : {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+  }
+}
 
-module.exports = { transport }
+let transporter = nodemailer.createTransport(config);
+
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: process.env.EMAIL, // sender address
+    to: process.env.TO, // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  //
+  // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
+  //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
+  //       <https://github.com/forwardemail/preview-email>
+  //
+}
+
+main().catch(console.error);
